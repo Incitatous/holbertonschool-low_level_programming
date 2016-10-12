1bad494e5be2bf562621317dc0d58469c29198db
Ian <dcianberlin@gmail.com>
2016-10-12 21:29:40 +0000

    task5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
-
+/**
+* main - This is a function that prints a string
+* Description: This function prints /
+* "Programming is like building a multilingual puzzle
+* Return: Returns 0
+*/
 int	main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
-	return(0);
+	return (0);
 }
