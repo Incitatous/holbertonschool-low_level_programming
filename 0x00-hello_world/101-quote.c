4b3eb5cd235e481e11a415cb6c7cd1062020f623
Ian <dcianberlin@gmail.com>
2016-10-12 22:39:16 +0000

    task10

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,10 @@
 * main - This is a function that prints a string
 * Description: This function prints /
 * and that piece of art is useful - Dora Korpar, 2015-10-19
-* Return: Returns 0
+* Return: Returns 1
 */
 int	main(void)
 {
-	write(1, "and that piece of art is useful - Dora Korpar, 2015-10-19", 5);
-	return (0);
+	write(1, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	return (1);
 }
