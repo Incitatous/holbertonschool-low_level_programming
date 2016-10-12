4c5ef1ce98bfe706e3133d98749ee32e4a932781
Ian <dcianberlin@gmail.com>
2016-10-12 21:43:46 +0000

    task6

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,12 @@
 #include <unistd.h>
-
-int	main (void)
+/**
+* main - This is a function that prints a string
+* Description: This function prints /
+* "with proper grammar, but the outcome is a piece of art
+* Return: Returns 0
+*/
+int	main(void)
 {
 	write(1, "with proper grammar, but the outcome is a piece of art,\n", 56);
-	return(0);
+	return (0);
 }
