0746a44782ce2bb54eeeff9efedaa5842d109721
Ian <dcianberlin@gmail.com>
2016-10-12 22:33:44 +0000

    task10

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+* main - This is a function that prints a string
+* Description: This function prints /
+* and that piece of art is useful - Dora Korpar, 2015-10-19
+* Return: Returns 0
+*/
+int	main(void)
+{
+	write(1, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	return (0);
+}
