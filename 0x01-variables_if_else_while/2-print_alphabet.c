db613073d0d1e8135d88e219d44512b61a8c5402
Ian <dcianberlin@gmail.com>
2016-10-13 17:07:04 +0000

    task3

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+* main - This is a function that prints a string
+* Description: This function prints /
+* the alphabet in lowercase
+* Return: Returns 0
+*/
+int	main(void)
+{
+	char c;
+	c = 'a';
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
