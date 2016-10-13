824020cf5163ac7a00e961a90651f57f126a0aa1
Ian <dcianberlin@gmail.com>
2016-10-13 17:24:59 +0000

    task6

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - This is a function that prints a string
+* Description: This function prints /
+* the base 10 numbers
+* Return: Returns 0
+*/
+int	main(void)
+{
+	printf("0123456789\n");
+	return (0);
+}
