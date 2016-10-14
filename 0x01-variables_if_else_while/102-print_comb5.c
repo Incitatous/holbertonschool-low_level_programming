26d4a570351105a2c6aa79f323efd2e3729fdfbd
Ian <dcianberlin@gmail.com>
2016-10-14 20:53:16 +0000

    task14

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+* main - This is a function that prints a string
+* Description: This function prints /
+* all different combination of two two-digit nb
+* Return: Returns 0
+*/
+int	main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 99; i++)
+	{
+		for (j = 0; j <= 99; j++)
+		{
+			if (j > i)
+			{ 
+			putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
+			putchar (' ');
+			putchar (j / 10 + '0');
+			putchar (j % 10 + '0');
+				if (!(j == 99 && i == 98))
+				{
+					putchar (',');
+					putchar (' ');
+				}
+			}
+		}
+	}
+	putchar ('\n');
+	return (0);
+}
+		
