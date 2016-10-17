0d1b6872fcb09c81267fe3467ce14a427590c1f6
Ian <dcianberlin@gmail.com>
2016-10-17 23:38:31 +0000

    task9

---
+++ 0x02_functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+* _abs - Absolute value
+* Description: This function prints /
+* from 00:00 to 23:59
+* @i: the input
+* Return: Returns 0
+*/
+void jack_bauer(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 23; i++)
+	{
+		for (j = 0; j <= 59; j++)
+		{
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar(':');
+			_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
