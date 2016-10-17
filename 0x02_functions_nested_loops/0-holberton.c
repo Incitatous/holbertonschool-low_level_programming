c586f40beddf393c415e974ee38ee31407d3960e
Ian <dcianberlin@gmail.com>
2016-10-17 15:39:25 +0000

    task1

---
+++ 0x02_functions_nested_loops/0-holberton.c
@@ -0,0 +1,22 @@
+/**
+* main - This is a function that prints a string
+* Description: This function prints /
+* base numbers separated by a comma and space
+* Return: Returns 0
+*/
+int _putchar(char c);
+
+int	main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
+}
