718c832e2b8b54bb53b4800818a3f7f0f7709f54
Ian <dcianberlin@gmail.com>
2016-10-18 16:35:55 +0000

    task6

--- 0x02_functions_nested_loops/5-sign.c
+++ 0x02_functions_nested_loops/5-sign.c
@@ -1,7 +1,8 @@
 /**
-* main - This is a function that prints a string
+* print_sign - This is a function that prints a string
 * Description: This function checks /
 * for lower case character
+* @n: holds the int
 * Return: Returns 0
 */
 #include "holberton.h"
@@ -13,10 +14,10 @@ int print_sign(int n)
 		return (1);
 	}
 	else if (n < 0)
-        {
-                _putchar('-');
-                return (-1);
-        }
+	{
+		_putchar('-');
+		return (-1);
+	}
 	else
 	{
 		_putchar('0');
