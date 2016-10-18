94de7b60fa9ba2faaadda365cc7308dbf0bf3e3b
Ian <dcianberlin@gmail.com>
2016-10-18 22:13:59 +0000

    task10

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,45 @@
+#include "holberton.h"
+/**
+* times_table - Absolute value
+* Description: This function prints /
+* time tables
+* Return: Returns 0
+*/
+void times_table(void)
+{
+	int i;
+	int j;
+	int k;
+
+	i = 0;
+	j = 0;
+	while (i <= 9)
+	{
+		while (j <= 9)
+		{
+			k = i * j;
+			if (k / 10 == 0)
+			{
+				_putchar(' ');
+				_putchar(k % 10 + '0');
+			}
+			else
+			{
+				_putchar(k / 10 + '0');
+				_putchar(k % 10 + '0');
+			}
+			if (j != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (j == 9)
+			{
+				_putchar('\n');
+			}
+			j++;
+		}
+		i++;
+		j = 0;
+	}
+}
