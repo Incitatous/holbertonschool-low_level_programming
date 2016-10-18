1c0110edeb42c2e7343c79ea066ca8af0a0d2e39
Ian <dcianberlin@gmail.com>
2016-10-18 20:20:45 +0000

    task11

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+* add - Adds two ints
+* Description: This function adds /
+* two integers and return the result
+* @i: first int
+* @j: second int
+* Return: Returns i or j
+*/
+int add(int i, int j)
+{
+	int k;
+
+	k = (i + j);
+	return (k);
+}
