5fd323ad71c915d26a2997e897330931d29b2752
Incitatous <dcianberlin@gmail.com>
2016-10-19 20:52:57 +0000

    task13

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -15,38 +15,34 @@ void print_times_table(int n)
 		{
 			for (j = 0; j <= n; j++)
 			{
-				k = i * j;
-				if (k / 10 == 0)
-				{
-					if (j == 0)
-						_putchar(k % 10 + '0');
-					else
-					{
-						_putchar(' ');
-						_putchar(k % 10 + '0');
-					}
-				}
-				else if (k < 100)
-				{
-					_putchar(k / 10 + '0');
-					_putchar(k % 10 + '0');
-				}
-				else
-				{
-					_putchar(k / 100 + '0');
-					_putchar(k / 10 % 10 + '0');
-					_putchar(k % 10 + '0');
-				}
-				if (j != n)
+				k = (i * j);
+				if (j != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
-					if (k < 100)
-						_putchar(' ');
 				}
-				if (j == n)
-					_putchar('\n');
+				if (k < 10 && j != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((k % 10) + '0');
+				}
+				else if (k >= 10 && k < 100)
+				{
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+				else if (k >= 100 && j != 0)
+				{
+					_putchar((k / 100) + '0');
+					_putchar((k / 10) % 10 + '0');
+					_putchar((k % 10) + '0');
+				}
+				else
+					_putchar((k % 10) + '0');
 			}
+			_putchar('\n');
 		}
 	}
 }
