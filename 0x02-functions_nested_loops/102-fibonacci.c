f75b5aa469150fb20dccda3f9680bbad14d900fa
Ian <dcianberlin@gmail.com>
2016-10-19 23:21:11 +0000

    task15

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+* main - main function
+* Description: This program adds /
+* the natural numbers up to 1024
+* Return: Returns 0
+*/
+
+int main(void)
+{
+	unsigned long count, l, o, k;
+
+	l = 1;
+	o = 2;
+	printf("1, 2");
+	for (count = 0 ; count < 49; count++)
+	{
+		{
+			k = o + l;
+			l = o;
+			o = k;
+				if (count != 49)
+					printf(", ");
+			printf("%lu", k);
+		}
+	}
+	printf("\n");
+	return (0);
+}
