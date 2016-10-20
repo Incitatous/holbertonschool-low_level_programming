2837aa9d1c11b3f84241cab08a812027ff6a8aaf
Ian <dcianberlin@gmail.com>
2016-10-20 21:54:56 +0000

    task10

--- 0x03-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,12 +13,12 @@ int main(void)
 	c = 1;
 	while (c <= 99)
 	{
-		if ((c % 3 == 0) && (c % 5 != 0))
+		if ((c % 5 == 0) && (c % 3 == 0))
+                        printf("FizzBuzz ");
+		else if ((c % 3 == 0) && (c % 5 != 0))
 			printf("Fizz ");
-		else if ((c % 5 == 0 && c % 3 != 0))
-			printf("Buzz ");
 		else if ((c % 5 == 0) && (c % 3 != 0))
-			printf("FizzBuzz ");
+			printf("Buzz ");
 		else
 		{
 			if (c > 9)
