8bd4fbafada57634d25b4b454763efdbd481ebe5
Ian <dcianberlin@gmail.com>
2016-10-20 21:40:10 +0000

    task10

---
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+/**
+* main - This is a function that prints a string
+* Description: This function prints /
+* Fizz, Buzz and Fizz Buzz when necessary
+* Return: Returns 0
+*/
+#include <unistd.h>
+#include <stdio.h>
+int main(void)
+{
+	int c;
+
+	c = 0;
+	while (c <= 99)
+	{
+		if ((c % 3 == 0) && (c % 5 != 0))
+			printf("Fizz ");
+		else if ((c % 5 == 0 && c % 3 != 0))
+			printf("Buzz ");
+		else if ((c % 5 == 0) && (c % 3 != 0))
+			printf("Fizz Buzz ");
+		else
+		{
+			if (c > 9)
+				putchar(c / 10 + '0');
+			putchar(c % 10 + '0');
+			printf(" ");
+		}
+		c++;
+	}
+	printf("Fizz Buzz\n");
+	return (0);
+}
