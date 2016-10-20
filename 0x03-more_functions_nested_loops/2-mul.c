40b929b5ec6d7c840ac6e7e441e531a4eec269e1
Ian <dcianberlin@gmail.com>
2016-10-20 16:42:29 +0000

    task3

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+/**
+* mul - This is a function multiplies
+* Description: This function multiplies /
+* two integers
+* @a: first int
+* @b: second int
+* Return: Returns 0
+*/
+#include "holberton.h"
+int mul(int a, int b)
+{
+	return (a * b);
+}
