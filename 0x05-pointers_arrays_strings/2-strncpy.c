bf1d51f5f124401bd537f6614485830e135d64e1
Ian <dcianberlin@gmail.com>
2016-10-27 18:04:16 +0000

    task3

---
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * _strncpy - copies two strings
+ * @src: source
+ * @dest: destination
+ * @n: number of bytes
+ * Return: Always 0.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
