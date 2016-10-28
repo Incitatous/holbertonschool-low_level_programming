49158a6211abe8d202dc8a710199d330b974b548
Ian <dcianberlin@gmail.com>
2016-10-28 17:47:18 +0000

    task10

---
+++ 0x05-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * print_number - check the code for Holberton School students.
+ * @n: holds the number
+ * Return: Always 0.
+ */
+void print_number(int n)
+{
+	long expo;
+	long temp;
+	long temp2;
+
+	expo = 1;
+	temp2 = n;
+	if (n < 0)
+	{
+		temp2 *= -1;
+		_putchar('-');
+	}
+	temp = temp2;
+	while ((temp / 10) > 0)
+	{
+		temp  = temp / 10;
+		expo *= 10;
+	}
+	while (expo > 0)
+	{
+		_putchar((temp2 / expo) % 10 + '0');
+		expo /= 10;
+	}
+}
