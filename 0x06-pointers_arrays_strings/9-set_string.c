bd18478f6f76d27fbd2ff6357c85b06c5baa434a
Ian <dcianberlin@gmail.com>
2016-11-01 22:56:33 +0000

    task9

---
+++ 0x06-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * set_string - sets value of ptr to char
+ * @s: pointer 1
+ * @tp: pointer 2
+ * Return: Always 0.
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
