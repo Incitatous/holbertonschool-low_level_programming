4a99943e4387d1c3c014508bfc8d1f1e4fa0f8ed
Ian <dcianberlin@gmail.com>
2016-11-01 20:15:57 +0000

    task6

--- 0x06-pointers_arrays_strings/5-strstr.c
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -30,6 +30,8 @@ char *_strstr(char *haystack, char *needle)
 	int i;
 
 	i = 0;
+	if (needle[0] == '\0')
+		return (&haystack[0]);
 	while (haystack[i] != '\0')
 	{
 		if (haystack[i] == needle[0])
