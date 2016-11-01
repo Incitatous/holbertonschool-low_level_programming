f31d666d8f32881bf7dd56c84ce52b49cd2aa57b
Ian <dcianberlin@gmail.com>
2016-11-01 20:09:27 +0000

    task6

---
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _strcmp - copies two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: Always 0.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] == s2[i] && s1[i] != '\0')
+	{
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}
+
+/**
+ * _strstr - finds a substr in str
+ * @needle: substring
+ * @haystack: string
+ * Return: first occurence of c in str
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+
+	i = 0;
+	while (haystack[i] != '\0')
+	{
+		if (haystack[i] == needle[0])
+		{
+			if (_strcmp(&needle[0], &haystack[i]) == 0)
+				return (&haystack[i]);
+		}
+		i++;
+	}
+	return (NULL);
+}
