a7b4920963dc9a3c07a0aa25a36814b5e64a6e2b
Ian <dcianberlin@gmail.com>
2016-11-02 22:12:44 +0000

    task6

---
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * check - looks for square root
+ * @i: checker
+ * @n: number passed
+ * Return: number x to the power y
+ */
+int check(int i, int n)
+{
+	if ((i * i) == n)
+		return (i);
+	if ((i * i) > n)
+		return (-1);
+	if ((i * i) < n)
+		return (check(i + 1, n));
+}
+
+/**
+ * _sqrt_recursion - returns square
+ * @n: number passed
+ * Return: number x to the power y
+ */
+int _sqrt_recursion(int n)
+{
+	return (check(1, n));
+}
