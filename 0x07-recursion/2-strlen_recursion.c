419fffd33cfafbd906468b74c7fcf18850c7272f
Ian <dcianberlin@gmail.com>
2016-11-02 18:27:14 +0000

    task3

---
+++ 0x07-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _strlen_recursion - returns len of str
+ * @s: pointer to string
+ * Return: length
+ */
+int _strlen_recursion(char *s)
+{
+	int i;
+
+	i = 0;
+	if (*s == '\0')
+		return (0);
+	i = 1 + _strlen_recursion(s + 1);
+	return (i);
+}
