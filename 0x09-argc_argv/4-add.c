8e9fa0a60041b6361db09b0e58e88ba16f2d10a5
Incitatous <dcianberlin@gmail.com>
2016-11-06 15:10:32 -0800

    task5

---
+++ 0x09-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Adds two positive int
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 1 if error
+ */
+int main(int argc, char *argv[])
+{
+	int i, add, res;
+
+	res = 0;
+	i = 1;
+	while (i < argc)
+	{
+		add = atoi(argv[i]);
+		if (add == 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		if (add > 0)
+			res += add;
+		i++;
+	}
+	printf("%d\n", res);
+	return (0);
+}
