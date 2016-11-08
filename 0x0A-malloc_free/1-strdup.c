bdff77c7f24f002860b3c153c9c8f795b3ed8bb7
Ian <dcianberlin@gmail.com>
2016-11-08 22:02:51 +0000

    task2

---
+++ 0x0A-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - duplicates a string
+ * @str: string to duplicate
+ * Return: pointer to array or NULL
+ */
+char *_strdup(char *str)
+{
+	char *my_array;
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	my_array = malloc(i * sizeof(str));
+	while (i >= 0)
+	{
+		my_array[i] = str[i];
+		i--;
+	}
+	return (my_array);
+	free(str);
+}
