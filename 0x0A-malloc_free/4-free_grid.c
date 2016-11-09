34ad2d59bb018df607085e33f4e003dadbaa28ea
Ian <dcianberlin@gmail.com>
2016-11-09 01:17:00 +0000

    test

---
+++ 0x0A-malloc_free/4-free_grid.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - allocates grid
+ * @grid: width
+ * @height: height
+ * Return: nothing
+ */
+void free_grid(int **grid, int height)
+{
+	free(grid);
+}
