abb38eaf317a8d73e97da0fe0c8623914dbc9601
Incitatous <dcianberlin@gmail.com>
2016-11-10 11:28:30 -0800

    fix

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -15,9 +15,9 @@ char *str_concat(char *s1, char *s2)
 	i = j = k = 0;
 /*	empty = "";*/
 	if (s1 == NULL)
-		s1 = "";
+		s1 = NULL;
 	if (s2 == NULL)
-		s2 = "";
+		s2 = NULL;
 	while (s1[i] != '\0')
 		i++;
 	while (s2[j] != '\0')
