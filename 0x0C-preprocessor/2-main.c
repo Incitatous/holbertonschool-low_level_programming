99fa1944f46e1bb6be0b174e7d98addb31f77beb
Ian <dcianberlin@gmail.com>
2016-11-14 18:45:17 +0000

    task3

--- 0x0C-preprocessor/2-main.c
+++ 0x0C-preprocessor/2-main.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
- * @argc: argument count
- * @argv: argument vector
+ * main - Prints filename.
  * Return: Always 0.
  */
-int main(int argc, char *argv[])
+int main(void)
 {
-	if (argc > 0)
-		printf("%s\n", argv[0]);
-	return (0);
+	printf("%s\n", __FILE__);
 }
