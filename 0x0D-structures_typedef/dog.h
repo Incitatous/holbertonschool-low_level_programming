b2cc803bbb5939898d492546d6aae6e3bb0b9df0
Ian <dcianberlin@gmail.com>
2016-11-14 21:59:09 +0000

    header file

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - SE
+ * @name: SE
+ * @age: SE
+ * @owner: SE
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+
+#endif
