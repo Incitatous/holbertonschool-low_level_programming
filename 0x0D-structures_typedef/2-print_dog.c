92bea990df4b603d68125e6d0890e642b708e96c
Incitatous <dcianberlin@gmail.com>
2016-11-14 17:49:35 -0800

    task3

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -14,10 +14,10 @@ void print_dog(struct dog *d)
 			printf("Name: %s\n", (*d).name);
 		else
 			printf("Name: (nil)\n");
-		printf("Age: %f\n", (*d).age)
+		printf("Age: %f\n", (*d).age);
 		if ((*d).owner != NULL)
 			printf("Owner: %s\n", (*d).owner);
 		else
-			rintf("Owner: (nil)\n");
+			printf("Owner: (nil)\n");
 	}
 }
