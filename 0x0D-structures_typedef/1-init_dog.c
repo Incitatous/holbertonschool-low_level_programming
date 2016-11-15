0c1af8b3441135deaab9e4dc72b2708ce362caba
Incitatous <dcianberlin@gmail.com>
2016-11-14 16:55:22 -0800

    fix

--- 0x0D-structures_typedef/1-init_dog.c
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -1,1 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - creates structure dog
+ * @name: SE
+ * @age: SE
+ * @owner: SE
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+
+typedef struct dog;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif
