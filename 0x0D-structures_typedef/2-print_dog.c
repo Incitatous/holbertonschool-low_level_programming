c2bea3166b8e0a7322510271eac1cc4e065cf8e0
Incitatous <dcianberlin@gmail.com>
2016-11-14 17:46:31 -0800

    task3

---
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_dog - prints dog
+ * @d: pointer to struct
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if ((*d).name != NULL)
+			printf("Name: %s\n", (*d).name);
+		else
+			printf("Name: (nil)\n");
+		printf("Age: %f\n", (*d).age)
+		if ((*d).owner != NULL)
+			printf("Owner: %s\n", (*d).owner);
+		else
+			rintf("Owner: (nil)\n");
+	}
+}
