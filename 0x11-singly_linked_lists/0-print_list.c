babe0040ff191357abe75474beade3fda0ff9986
Incitatous <dcianberlin@gmail.com>
2016-11-28 13:28:54 -0800

    task1

---
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * print_list - prints a list_t list
+ * @h: pointer to struct
+ * Return: length
+ */
+size_t print_list(const list_t *h)
+{
+	unsigned int length;
+
+	length = 0;
+	while (h != NULL)
+	{
+		++length;
+		printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+	}
+	return (length);
+}
