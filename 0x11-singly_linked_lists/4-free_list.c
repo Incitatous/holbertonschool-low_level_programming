1c6bf72e596ca9b5b975494cf237804e7d56af57
Incitatous <dcianberlin@gmail.com>
2016-11-28 17:54:53 -0800

    freedom at last?

--- 0x11-singly_linked_lists/4-free_list.c
+++ 0x11-singly_linked_lists/4-free_list.c
@@ -5,15 +5,17 @@
 /**
  * free_list - frees a list_t list
  * @head: linked list
- * Return: NULL
+ * Return: Freedom!
  */
 void free_list(list_t *head)
 {
 	list_t *tmp;
+
 	while (head != NULL)
 	{
 		tmp = head;
 		head = head->next;
 		free(tmp);
 	}
+	free(head);
 }
