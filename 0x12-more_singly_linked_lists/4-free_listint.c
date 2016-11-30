3445ec4cc79dde06f01b774b64c2b86c700b4933
Incitatous <dcianberlin@gmail.com>
2016-11-30 12:06:15 -0800

    task5

---
+++ 0x12-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * free_listint - frees a listint_t list
+ * @head: linked list
+ * Return: No return
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+	free(head);
+}
