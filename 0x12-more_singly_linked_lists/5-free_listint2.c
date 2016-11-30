d144317a395d630d50a1d87ccfbf6bed831836a9
Incitatous <dcianberlin@gmail.com>
2016-11-30 12:31:34 -0800

    task6

---
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * free_listint2 - frees a listint_t list
+ * @head: linked list
+ * Return: No return
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+	listint_t *store;
+
+	tmp = *head;
+	while (tmp->next != NULL)
+	{
+		store = tmp;
+		tmp = store->next;
+		free(store);
+	}
+	free(tmp);
+}
