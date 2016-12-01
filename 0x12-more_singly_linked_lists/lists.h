5200a30efd5cc360415e6a90b2c937676d1a45e8
Incitatous <dcianberlin@gmail.com>
2016-12-01 13:19:17 -0800

    conceding to Betty

--- 0x12-more_singly_linked_lists/lists.h
+++ 0x12-more_singly_linked_lists/lists.h
@@ -10,8 +10,8 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
@@ -23,6 +23,6 @@ void free_listint2(listint_t **head);
 int pop_listint(listint_t **head);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index, int n);
+listint_t *insert_nodeint_at_index(listint_t, unsigned int, int);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 #endif
