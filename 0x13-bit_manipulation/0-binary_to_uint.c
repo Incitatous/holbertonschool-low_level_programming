8400bfca9a06ed09544fb8f6777bbdda0ce0dccf
Incitatous <dcianberlin@gmail.com>
2016-12-05 16:14:39 -0800

    checker test

---
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ *
+ * Return: Converted number or 0
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int i, power, res;
+
+	i = res = 0;
+	power = 1;
+	if (!(b[i] == '0' || b[i] == '1') || (!b))
+		return (0);
+	while (b[i] != '\0')
+	{
+		if (!(b[i] == '0' || b[i] == '1'))
+			return (0);
+		i++;
+	}
+	while (i > 0)
+	{
+		power *= 2;
+		if (b[i] == '1')
+			res += power;
+		i--;
+	}
+	return (res);
+}
