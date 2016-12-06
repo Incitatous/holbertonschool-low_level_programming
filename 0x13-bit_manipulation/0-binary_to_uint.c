e11f05247cd1f4f3b38bd140b2e16c0c51548a72
Incitatous <dcianberlin@gmail.com>
2016-12-05 16:43:23 -0800

    back to where it was

--- 0x13-bit_manipulation/0-binary_to_uint.c
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -11,11 +11,11 @@ unsigned int binary_to_uint(const char *b)
 
 	i = res = 0;
 	power = 1;
-	if ((b[i] != '0' || b[i] != '1') || (!b))
+	if (!(b[i] == '0' || b[i] == '1') ||(!b))
 		return (0);
 	while (b[i] != '\0')
 	{
-		if ((b[i] != '0' || b[i] != '1'))
+		if (!(b[i] == '0' || b[i] == '1'))
 			return (0);
 		i++;
 	}
