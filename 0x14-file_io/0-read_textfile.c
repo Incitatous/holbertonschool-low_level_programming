8d3e928c2699598d8bf16b5e7ae99959aafc3eac
Incitatous <dcianberlin@gmail.com>
2016-12-08 11:58:04 -0800

    task1

---
+++ 0x14-file_io/0-read_textfile.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buffer;
+	int fileDescriptor;
+	ssize_t count, checkCount;
+	
+	if (!filename)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (!buffer)
+	{
+		free(buffer);
+		return (0);
+	}
+	fileDescriptor = open(filename, O_RDONLY);
+	if (fileDescriptor == -1)
+		return (0);
+	count = read(fileDescriptor, buffer, letters);
+	if (count == -1)
+		return (0);
+	/* same as write(1, file, number of characters)*/
+	checkCount = write(STDOUT_FILENO, buffer, count);
+	if (checkCount == -1 || (count != checkCount)
+		return (0);
+	close(fileDescriptor);
+	return (checkCount);
+}
