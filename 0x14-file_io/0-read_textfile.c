01417b8d0caf0c5c42f8b289a78d505ee8c7e008
Incitatous <dcianberlin@gmail.com>
2016-12-08 12:19:21 -0800

    fix?

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -10,20 +10,25 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	buffer = malloc(sizeof(char) * letters);
 	if (!buffer)
 	{
-		free(buffer);
 		return (0);
 	}
 	fileDescriptor = open(filename, O_RDONLY);
 	if (fileDescriptor == -1)
+	{
+		free(buffer);
 		return (0);
+	}
 	count = read(fileDescriptor, buffer, letters);
 	if (count == -1)
+	{
+		free(buffer);
 		return (0);
+	}
+	free(buffer);
 	/* same as write(1, file, number of characters)*/
 	checkCount = write(STDOUT_FILENO, buffer, count);
-	if (checkCount == -1 || (count != checkCount)
+	if (checkCount == -1 || (count != checkCount))
 		return (0);
-	free(buffer);
 	close(fileDescriptor);
 	return (checkCount);
 }
