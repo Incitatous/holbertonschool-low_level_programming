730f269d9e920247c4cc0732b97d063a2b105c3f
Incitatous <dcianberlin@gmail.com>
2016-12-08 12:37:29 -0800

    betty fix

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -11,7 +11,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buffer;
 	int fileDescriptor;
 	ssize_t count, checkCount;
-	
+
 	if (!filename)
 		return (0);
 	buffer = malloc(sizeof(char) * letters);
