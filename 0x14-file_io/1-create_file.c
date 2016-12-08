6219e0ba1b3e8593728fa0e8a94e3c15c1cf3004
Incitatous <dcianberlin@gmail.com>
2016-12-08 14:57:35 -0800

    fix?

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -8,19 +8,22 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fileDescriptor, i;
+	int fileDescriptor, i, count;
 
 	i = 0;
 	if (!filename)
 		return (-1);
-	fileDescriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IREAD | S_IWRITE);
+	fileDescriptor = open(filename, O_RDWR | O_CREAT
+	| O_TRUNC, S_IREAD | S_IWRITE);
 	if (fileDescriptor == -1)
 		return (-1);
 	if (!text_content)
 		return (-1);
 	while (text_content[i] != '\0')
 		i++;
-	write(STDIN_FILENO, text_content, i);
+	count = write(STDIN_FILENO, text_content, i);
+	if (count == -1)
+		return (-1);
 	close(fileDescriptor);
 	return (1);
 }
