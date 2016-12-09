689792d7808ce0ac003236cf916d52cc0698f31f
Incitatous <dcianberlin@gmail.com>
2016-12-08 21:14:46 -0800

    fix

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -12,7 +12,7 @@ int main(int ac, char **av)
 	char buffer[BUFSIZE];
 
 	if (ac != 3)
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to"), exit(97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	if (!av[1])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 	source = open(av[1], O_RDONLY);
