aba2bec8f5eadc461895bcca0eba0cb148f23ecd
Incitatous <dcianberlin@gmail.com>
2016-12-08 21:07:12 -0800

    task4

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -13,9 +13,13 @@ int main(int ac, char **av)
 
 	if (ac != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to"), exit(97);
+	if (!av[1])
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 	source = open(av[1], O_RDONLY);
 	if (source == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+	if (!av[2])
+		dprintf(STDERR_FILENO, "Can't write to %s\n", av[2]), exit(99);
 	dest = open(av[2], O_WRONLY | O_TRUNC | O_CREAT, S_IREAD
 	| S_IWRITE | S_IRGRP | S_IWGRP | S_IROTH);
 	if (dest == -1)
