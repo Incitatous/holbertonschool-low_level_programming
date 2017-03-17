689f63995219a6639e98dc041e7b0296d466f234
Incitatous <dcianberlin@gmail.com>
2017-03-16 18:21:00 -0700

    task9

---
+++ 0x1C-binary_trees/9-binary_tree_height.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_height - Gives the size of a (sub)tree
+ *
+ * @node: SE
+ * Return: New node
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	int left, right;
+
+	if (!tree || (!tree->left && !tree->right))
+		return (0);
+	left = 1 + binary_tree_height(tree->left);
+	right = 1 + binary_tree_height(tree->right);
+	if (left > right)
+		return left;
+	return right;
+}
