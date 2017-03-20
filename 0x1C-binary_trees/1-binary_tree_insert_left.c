9d32ab8729b12ea7b98e2503a71dfe99804b82b6
Incitatous <dcianberlin@gmail.com>
2017-03-20 15:59:50 -0700

    NULL check for parent

--- 0x1C-binary_trees/1-binary_tree_insert_left.c
+++ 0x1C-binary_trees/1-binary_tree_insert_left.c
@@ -11,6 +11,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *newNode;
 	binary_tree_t *tmp;
 
+	if (!parent)
+		return (NULL);
 	newNode = malloc(sizeof(binary_tree_t));
 	if (!newNode)
 		return (NULL);
