b781716741f02f1c5a3c4ff73b22ca6e9ea8731b
Incitatous <dcianberlin@gmail.com>
2017-04-14 11:59:00 -0700

    first commit

---
+++ 0x1D-search_algorithms/0-linear.c
@@ -0,0 +1,25 @@
+#include "search_algos.h"
+/**
+ * linear_search - Linear search algorithm
+ *
+ * @array: SE
+ * @size: SE
+ * @value: SE
+ * Return: Always EXIT_SUCCESS
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	i = 0;
+	if (!array)
+		return (-1);
+	while (i <= size)
+	{
+		printf("Value checked array [%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
---
+++ 0x1D-search_algorithms/1-binary.c
@@ -0,0 +1,39 @@
+#include "search_algos.h"
+/**
+ * binary_search - Binary search for sorted arrays
+ *
+ * @array: SE
+ * @size: SE
+ * @value: SE
+ * Return: Always EXIT_SUCCESS
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t left, right, middle, i;
+
+	left = 0;
+	right = size - 1;
+	if (!array)
+		return (-1);
+	if (left >= right)
+		return (left);
+	while (left <= right)
+	{
+		i = left;
+		printf("Searching in array: ");
+		while (i < right)
+		{
+			printf("%d, ", array[i]);
+			i++;
+		}
+		printf("%d\n", array[i]);
+		middle = (right + left) / 2;
+		if (array[middle] == value)
+			return (middle);
+		if (array[middle] < value)
+			left = middle + 1;
+		else
+			right = middle;
+	}
+	return (-1);
+}
